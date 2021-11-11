8ce045ee8b8fdaa7054d004f342a09f0fa25c430
kokokojo2 <foxgunplay@gmail.com>
2021-11-11 12:27:22 +0200

    unused comments removed

--- main.cpp
+++ main.cpp
@@ -60,7 +60,6 @@ unsigned int vertexBufferObjConfiguration() {
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 
     return vertexBufferObject;
-    //glBindBuffer(GL_ARRAY_BUFFER, 0);
 }
 
 unsigned int elementBufferObjConfiguration() {
